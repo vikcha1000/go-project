a67353f396ffb426e50cba5ba64e90f7b9666785
zhustova <viktoriya.zhustova@sbermegamarket.ru>
2025-05-29 17:13:08 +0300

    Улучшила ошибки

--- internal/service/task/handler.go
+++ internal/service/task/handler.go
@@ -26,12 +26,12 @@ func (h *TaskHandler) SetupAPI(r fiber.Router) {
 	group.Get("/:id", h.GetTaskByID)
 }
 
-//CreateTask создает и возвращает задачу
+// CreateTask создает и возвращает задачу
 func (h *TaskHandler) CreateTask(c *fiber.Ctx) error {
 	var req CreateTaskRequest
 	if err := c.BodyParser(&req); err != nil {
 		h.log.Warn("Failed to parse request", zap.Error(err))
-		return fiber.NewError(fiber.StatusBadRequest, "Invalid request body")
+		return fiber.NewError(fiber.StatusBadRequest, "Некорректное тело запроса")
 	}
 
 	if err := h.validate.Struct(req); err != nil {
@@ -39,11 +39,16 @@ func (h *TaskHandler) CreateTask(c *fiber.Ctx) error {
 		return fiber.NewError(fiber.StatusBadRequest, err.Error())
 	}
 
-	if err := h.service.ValidateUsersExist(c.Context(), req.AuthorID, req.ExecutorID); err != nil {
-		h.log.Warn("User validation failed", 
-			zap.Uint("authorID", req.AuthorID),
+	if err := h.service.ValidateUsersExist(c.Context(), req.AuthorID); err != nil {
+		h.log.Warn("User validation failed: author not exist",
+			zap.Uint("authorID", req.AuthorID))
+		return fiber.NewError(fiber.StatusBadRequest, "Автор - несуществующий юзер")
+	}
+
+	if err := h.service.ValidateUsersExist(c.Context(), req.ExecutorID); err != nil {
+		h.log.Warn("User validation failed: executor not exist",
 			zap.Uint("executorID", req.ExecutorID))
-		return fiber.NewError(fiber.StatusBadRequest, "User not found")
+		return fiber.NewError(fiber.StatusBadRequest, "Исполнитель - несуществующий юзер")
 	}
 
 	task, err := h.service.CreateTask(c.Context(), req)
@@ -55,17 +60,18 @@ func (h *TaskHandler) CreateTask(c *fiber.Ctx) error {
 	return c.Status(fiber.StatusCreated).JSON(ToTaskResponse(*task))
 }
 
-//GetTaskByID возвращает задачу по id
+// GetTaskByID возвращает задачу по id
 func (h *TaskHandler) GetTaskByID(c *fiber.Ctx) error {
 	id, err := c.ParamsInt("id")
 	if err != nil {
-		return fiber.NewError(fiber.StatusBadRequest, "Invalid task ID")
+		h.log.Warn("Invalid task ID")
+		return fiber.NewError(fiber.StatusBadRequest, "Некорректный id")
 	}
 
 	task, err := h.service.GetTaskByID(c.Context(), uint(id))
 	if err != nil {
 		h.log.Warn("Task not found", zap.Uint("id", uint(id)))
-		return fiber.NewError(fiber.StatusNotFound, "Task not found")
+		return fiber.NewError(fiber.StatusNotFound, "Задача не найдена")
 	}
 
 	return c.JSON(ToTaskResponse(*task))
